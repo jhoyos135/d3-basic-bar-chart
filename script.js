d0ba04330122a8dfdb1fb23d94a5258aab77a025
Julian Hoyos <jhoyos135@gmail.com>
2018-08-11 13:03:12 -0400

    initial commit of basic chart with some transitions

---
+++ script.js
@@ -0,0 +1,62 @@
+// d3 chart
+let myData = [150,125,250,440,500,250,300,650, 410, 250, 720, 400];
+
+let height = 600;
+let width = 400;
+
+let tooltip = d3.select('#chart').append('div')
+  .style('position','absolute')
+  .style('background','#f4f4f4')
+  .style('padding','5px 15px')
+  .style('border','1px solid #333')
+  .style('border-radius','5px')
+  .style('opacity','0');
+
+let yScale = d3.scale.linear()
+.domain([0, d3.max(myData)])
+.range([0, height]);
+
+let xScale = d3.scale.ordinal()
+.domain(d3.range(0, myData.length))
+.rangeBands([0, width]);
+
+let colors = d3.scale.linear()
+.domain([0, myData.length])
+.range(['#429ef4', '#c9304c']);
+
+
+d3.select('#chart').append('svg')
+.attr('width', width)
+.attr('height', height)
+.selectAll('rect')
+  .data(myData)
+  .enter().append('rect')
+    .style('fill', (d, i) => {
+      return colors(i);
+    })
+    .attr('width', xScale.rangeBand())
+    .attr('height', (d) => {
+      return yScale(d);
+    })
+    .attr('x', (d, i) => {
+      return xScale(i);
+    })
+    .attr('y', (d) => {
+      return height - yScale(d);
+    })
+
+    .on('mouseover', (d) => {
+      tooltip.transition()
+      .style('opacity', 1);
+      
+      tooltip.html(d)
+      .style('left', (d3.event.pageX)+'px')
+      .style('top', (d3.event.pageY)+'px' );
+      d3.select(this).style('opacity', 0.5);
+
+    })
+    .on('mouseout', (d) => {
+      tooltip.transition()
+        .style('opacity', 0);
+        d3.select(this).style('opacity', 1);
+    });
